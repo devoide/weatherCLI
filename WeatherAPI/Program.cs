a07a23c9317c7e5dd8d5334e7e333a6052f5c2fd
devoide <chefmocci@gmail.com>
2024-09-10 22:42:45 +0200

    Add project files.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace WeatherAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args) //Task is to allow synchronous stuff ykyk, it cannot be void.
+        {
+            string q = "Regensdorf";
+
+            if (args.Length > 0) 
+            {
+                q = args[0];
+            }
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://api.weatherapi.com/v1/forecast.json?key=67b99d2eaabe430cb15195631240309&q=" + q + "&days=1&aqi=no&alerts=no");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            
+            dynamic joResponse = JObject.Parse(responseBody);
+            string name = joResponse.location.name;
+            string country = joResponse.location.country;
+            string tempC = joResponse.current.temp_c.ToString();
+            string conditionText = joResponse.current.condition.text;
+
+
+
+            Console.WriteLine(name + ", " + country + ": " + tempC + "C, " + conditionText);
+        }
+    }
+}
---
+++ WeatherAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WeatherAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1495c711-7725-46d6-b895-90efd57ed75c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
